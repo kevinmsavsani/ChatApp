3cd1c368a56c4492b2a7c76f9642a00c318d585a
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-09 01:12:37 +0530

    Reducers

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -28,3 +28,24 @@ const message = (state, action) => {
             return state
     }
 }
+
+const messages = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_MESSAGE':
+            if (state.map(m => m.id).includes(action.id)) {
+                return state;
+            }else{
+                return [
+                ...state,
+                message(undefined, action)
+                ]
+            }
+        case 'SEND_MESSAGE':
+            return [
+                ...state,
+                message(undefined, action)
+            ]
+        default:
+            return state
+    }
+};
